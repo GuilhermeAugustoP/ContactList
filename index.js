32ecd5c3abb69c904c91ac861e8a6226ec94177c
GuilhermeAugustoP <guilherme.augustovp@gmail.com>
2022-03-16 23:16:42 -0300

    Add post and get methods

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    const contact = fs.readFileSync('./Contacts.json', {encoding: "utf8"})
+    //const contactList = JSON.stringify(contact)
+    res.send(contact)
+})
+app.post('/', function (req, res){
+    console.log(req.body)
+    const contactList = JSON.stringify(req.body)
+    const contact =fs.appendFileSync('./Contacts.json',contactList, {enconding:'utf8'})
+    res.send('200')
 })
 
 app.listen(3000)
