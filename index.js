62f7906d0098febf68e5746b4486cee567653616
GuilhermeAugustoP <guilherme.augustovp@gmail.com>
2022-04-09 00:57:14 -0300

    Create put and get HTTP method

--- index.js
+++ index.js
@@ -7,19 +7,37 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
-app.get('/', function (req, res) {
+app.get('/api/contacts/', function (req, res) { //Read contact list
     const contact = fs.readFileSync('./Contacts.json', {encoding: "utf8"})
-    //const contactList = JSON.stringify(contact)
-    res.send(contact)
+    res.status(200).send(contact)
 })
-app.post('/', function (req, res){
+app.post('/api/contacts/', function (req, res) { //Add contact to list
     const contactRead = fs.readFileSync('./Contacts.json', {encoding: "utf8"})
     const contactObj = JSON.parse(contactRead)
 
     contactObj.push(req.body)
     const contactAdd = JSON.stringify(contactObj)
     fs.writeFileSync('./Contacts.json',contactAdd, {enconding:'utf8'})
-    res.send('200')
+    res.status(200).send(contactAdd)
+})
+app.get('/api/contacts/:name', function (req, res) { //Read an specific contact
+  const contact = require('./Contacts.json')
+  const contactInfo = contact.find( cont => cont.name === req.params.name)
+  if (contactInfo) res.status(200).send(contactInfo)
+  else res.status(404).send('We could not find the contact.')
+})
+app.put('/api/contacts/:name', function (req, res) { //edit an specific contact name or number
+  const contact = require('./Contacts.json')
+  const contactInfo = contact.find( cont => cont.name === req.params.name)
+  if(contactInfo) {
+    contactInfo.name = req.body?.name || contactInfo.name
+    contactInfo.number = req.body?.number || contactInfo.number
+    const editContact = JSON.stringify(contact)
+    fs.writeFileSync('./Contacts.json',editContact, {enconding:'utf8'})
+    res.status(200).send(contactInfo)
+  }
+    else res.status(404).send('We could not find the contact.')
 })
 
-app.listen(3000)
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Listening on port ${port}...`))
